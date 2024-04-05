40fde09af33a93c43477b18d0b19e6cd893087f9
Tetyana <tetyana.chouteau@gmail.com>
2024-04-05 14:33:03 +0200

    testlog; exe1-dates by min bill, billesUI; exe2-admin navigate login; exe3-jpg newbill; exe4

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,7 +1,18 @@
-import { formatDate } from '../app/format.js'
+//composant complexe qui gère et les interactions
+// pour afficher, éditer et gérer les factures 
+//dans le tableau de bord d'administration. 
+//Elle utilise des méthodes asynchrones pour 
+//interagir avec le magasin de données et 
+//effectuer des opérations de update et de récupération de données.
+
+import {
+  formatDate
+} from '../app/format.js'
 import DashboardFormUI from '../views/DashboardFormUI.js'
 import BigBilledIcon from '../assets/svg/big_billed.js'
-import { ROUTES_PATH } from '../constants/routes.js'
+import {
+  ROUTES_PATH
+} from '../constants/routes.js'
 import USERS_TEST from '../constants/usersTest.js'
 import Logout from "./Logout.js"
 
@@ -16,7 +27,7 @@ export const filteredBills = (data, status) => {
       }
       /* istanbul ignore next */
       else {
-        // in prod environment
+        // Vérifie si l'environnement est de production
         const userEmail = JSON.parse(localStorage.getItem("user")).email
         selectCondition =
           (bill.status === status) &&
@@ -27,12 +38,13 @@ export const filteredBills = (data, status) => {
     }) : []
 }
 
+// Fonction pour générer une carte de facture
 export const card = (bill) => {
   const firstAndLastNames = bill.email.split('@')[0]
   const firstName = firstAndLastNames.includes('.') ?
     firstAndLastNames.split('.')[0] : ''
   const lastName = firstAndLastNames.includes('.') ?
-  firstAndLastNames.split('.')[1] : firstAndLastNames
+    firstAndLastNames.split('.')[1] : firstAndLastNames
 
   return (`
     <div class='bill-card' id='open-bill${bill.id}' data-testid='open-bill${bill.id}'>
@@ -52,9 +64,12 @@ export const card = (bill) => {
   `)
 }
 
+// Fonction pour générer des cartes de factures à partir d'un tableau de factures
 export const cards = (bills) => {
   return bills && bills.length ? bills.map(bill => card(bill)).join("") : ""
 }
+
+// Fonction pour obtenir le statut en fonction de l'index
 
 export const getStatus = (index) => {
   switch (index) {
@@ -66,18 +81,39 @@ export const getStatus = (index) => {
       return "refused"
   }
 }
-
+//Classe principale exportée
 export default class {
-  constructor({ document, onNavigate, store, bills, localStorage }) {
+  constructor({
+    document,
+    onNavigate,
+    store,
+    bills,
+    localStorage
+  }) {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
+
+   // Le constructeur utilise jQuery pour sélectionner des éléments HTML 
+   //avec les identifiants #arrow-icon1, #arrow-icon2, et #arrow-icon3, 
+   //qui représentent probablement des icônes fléchées. Il ajoute ensuite 
+   //des écouteurs d'événements click à ces éléments. Lorsque l'un de ces 
+   //éléments est cliqué, il appelle la méthode handleShowTickets avec 
+   //l'argument correspondant (1, 2, ou 3) ainsi que les factures (bills).
+
+//Instanciation de Logout : Le constructeur instancie également la classe 
+//Logout avec les dépendances localStorage et onNavigate.
+// la gestion des événements liés aux icônes de flèche et de l'initialisation de 
+//la fonctionnalité de déconnexion via l'instanciation de la classe Logout.
     $('#arrow-icon1').click((e) => this.handleShowTickets(e, bills, 1))
     $('#arrow-icon2').click((e) => this.handleShowTickets(e, bills, 2))
     $('#arrow-icon3').click((e) => this.handleShowTickets(e, bills, 3))
-    new Logout({ localStorage, onNavigate })
+    new Logout({
+      localStorage,
+      onNavigate
+    })
   }
-
+  // Gère le clic sur l'icône de l'œil pour afficher la modale
   handleClickIconEye = () => {
     const billUrl = $('#icon-eye-d').attr("data-bill-url")
     const imgWidth = Math.floor($('#modaleFileAdmin1').width() * 0.8)
@@ -85,32 +121,52 @@ export default class {
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
 
+  // Gère l'édition d'une facture, update son statut et affiche
+  //formulaire d'édition
   handleEditTicket(e, bill, bills) {
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
       console.log("handleEditTicket" + this.counter);
       bills.forEach(b => {
-        $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
+        $(`#open-bill${b.id}`).css({
+          background: '#0D5AE5'
+        })
       })
-      $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
+      $(`#open-bill${bill.id}`).css({
+        background: '#2A2B35'
+      })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
-      $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
+      $('.vertical-navbar').css({
+        height: '150vh'
+      })
+      this.counter++
     } else {
       console.log("handleEditTicket" + this.counter);
-      $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
+      $(`#open-bill${bill.id}`).css({
+        background: '#0D5AE5'
+      })
 
       $('.dashboard-right-container div').html(`
         <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
       `)
-      $('.vertical-navbar').css({ height: '120vh' })
-      this.counter ++
+      $('.vertical-navbar').css({
+        height: '120vh'
+      })
+      this.counter++
     }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
   }
+
+  // exe4 Gère la soumission du formulaire d'acceptation ou de refus d'une facture
+  //Je suis connecté en tant qu'administrateur RH, je déplie une liste de tickets
+  // (par exemple : statut "validé"), je sélectionne un ticket, puis 
+  //je déplie une seconde liste (par exemple : statut "refusé"),
+  // je ne peux plus sélectionner un ticket de la première liste. 
+// pourvoir déplier plusieurs listes, et consulter les tickets de chacune
+// des deux listes.
 
   handleAcceptSubmit = (e, bill) => {
     const newBill = {
@@ -131,24 +187,34 @@ export default class {
     this.updateBill(newBill)
     this.onNavigate(ROUTES_PATH['Dashboard'])
   }
-
+  //Gère l'affichage des factures en fonction du statut sélectionné (en attente, acceptée, refusée).
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
     if (this.counter % 2 === 0) {
       console.log("handleShowTickets" + this.counter);
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
+      $(`#arrow-icon${this.index}`).css({
+        transform: 'rotate(0deg)'
+      })
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
+      this.counter++
     } else {
       console.log("handleShowTickets" + this.counter);
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
+      $(`#arrow-icon${this.index}`).css({
+        transform: 'rotate(90deg)'
+      })
       $(`#status-bills-container${this.index}`)
         .html("")
-      this.counter ++
+      this.counter++
     }
-
+    //pour chaque facture présente dans le tableau bills, 
+    //un gestionnaire de clic est ajouté à l'élément HTML 
+    //ayant l'ID open-bill${bill.id} dans le conteneur spécifique 
+    //#status-bills-container${this.index}. 
+    //Lorsque cet élément est cliqué,
+    // la méthode handleEditTicket est appelée avec 
+    //les paramètres appropriés (l'événement, la facture et le tableau complet des factures).
     bills.forEach(bill => {
       $(`#status-bills-container${this.index} #open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
@@ -156,37 +222,41 @@ export default class {
     return bills
 
   }
-
+  //Récupère toutes les factures de tous les utilisateurs 
+  //à partir du magasin (store).
   getBillsAllUsers = () => {
     if (this.store) {
       return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot
-        .map(doc => ({
-          id: doc.id,
-          ...doc,
-          date: doc.date,
-          status: doc.status
-        }))
-        return bills
-      })
-      .catch(error => {
-        throw error;
-      })
+        .bills()
+        .list()
+        .then(snapshot => {
+          const bills = snapshot
+            .map(doc => ({
+              id: doc.id,
+              ...doc,
+              date: doc.date,
+              status: doc.status
+            }))
+          return bills
+        })
+        .catch(error => {
+          throw error;
+        })
     }
   }
-
+  //update les données d'une facture dans le magasin.
   // not need to cover this function by tests
   /* istanbul ignore next */
   updateBill = (bill) => {
     if (this.store) {
-    return this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: bill.id})
-      .then(bill => bill)
-      .catch(console.log)
+      return this.store
+        .bills()
+        .update({
+          data: JSON.stringify(bill),
+          selector: bill.id
+        })
+        .then(bill => bill)
+        .catch(console.log)
     }
   }
 }
